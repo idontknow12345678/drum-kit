093f9496d368f0f0c8280efec9bda98b2f02eca0
idontknow12345678 <53381869+idontknow12345678@users.noreply.github.com>
2019-07-30 17:27:09 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,30 +9,37 @@ function verify(letter){
     case "w":
     var audio = new Audio('sounds/crash.mp3');
     audio.play();
+    buttonAnimation(letter);
       break;
     case "a":
     var audio1 = new Audio('sounds/kick-bass.mp3');
     audio1.play();
+    buttonAnimation(letter)
       break;
     case "s":
     var audio2 = new Audio('sounds/snare.mp3');
     audio2.play();
+    buttonAnimation(letter)
       break;
     case "d":
     var audio3 = new Audio('sounds/tom-1.mp3');
     audio3.play();
+    buttonAnimation(letter)
       break;
     case "j":
     var audio4 = new Audio('sounds/tom-2.mp3');
     audio4.play();
+    buttonAnimation(letter)
       break;
     case "k":
     var audio5 = new Audio('sounds/tom-3.mp3');
     audio5.play();
+    buttonAnimation(letter)
       break;
     case "l":
     var audio6 = new Audio('sounds/tom-4.mp3');
     audio6.play();
+    buttonAnimation(letter)
 
       break;
     default: console.log(letter);
@@ -43,3 +50,14 @@ document.addEventListener("keydown", function(e){
   var letter = e.key;
   verify(letter);
 });
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+
+
+}
